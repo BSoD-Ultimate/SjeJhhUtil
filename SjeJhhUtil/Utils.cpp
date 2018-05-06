0dccee38f088002a85ab04b88c4168998f8871bb
bsod_ultimate <sunjinkuan@hotmail.com>
2018-05-07 00:48:08 +0800

    implement extracting "SJE.JHH" archive files functionality.

--- SjeJhhUtil/Utils.cpp
+++ SjeJhhUtil/Utils.cpp
@@ -14,5 +14,31 @@ namespace SjeJhhUtil
             return ss.str();
         }
 
+        FileHandle::FileHandle(const std::wstring& filename, const std::wstring& mode)
+            : m_filePath(filename)
+            , m_openMode(mode)
+            , m_fileHandle(NULL)
+        {
+            m_fileHandle = _wfopen(m_filePath.c_str(), mode.c_str());
+            int error = errno;
+            assert(m_fileHandle);
+
+            if (!m_fileHandle)
+            {
+                std::ostringstream ss;
+                ss << "Could not open file \"" << m_filePath << "\" ! errorCode = " << error;
+                throw std::runtime_error(ss.str());
+            }
+        }
+        FileHandle::~FileHandle()
+        {
+            fclose(m_fileHandle);
+        }
+
+        FILE* FileHandle::Get() const
+        {
+            return m_fileHandle;
+        }
+
     }
 }
